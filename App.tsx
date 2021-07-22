d3364178bed7d345190ddc45d8a2ce7b0fd2d2be
Jan Kremen <kremenh@gmail.com>
2021-07-22 12:50:10 +0200

    reproduce tab indicator position bug

--- App.tsx
+++ App.tsx
@@ -1,12 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useCallback} from 'react';
+import {Animated, StyleSheet, Text, useWindowDimensions, View} from 'react-native';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+
+const FirstRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#ff4081' }} />
+);
+
+const SecondRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#673ab7' }} />
+);
+
+const ThirdRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#42b73a' }} />
+);
+const FourthRoute = () => (
+  <View style={{ flex: 1, backgroundColor: '#d7ac25' }} />
+);
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+  third: ThirdRoute,
+  fourth: FourthRoute,
+});
 
 export default function App() {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'First' },
+    { key: 'second', title: 'Second' },
+    { key: 'third', title: 'Third' },
+    { key: 'fourth', title: 'Fourth' },
+  ]);
+  const layout = useWindowDimensions();
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{width: layout.width}}
+        renderTabBar={props => (<TabBar {...props} indicatorStyle={{backgroundColor: 'black', height: '100%'}}/>)}
+      />
     </View>
   );
 }
@@ -15,7 +50,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
     justifyContent: 'center',
+    marginTop: 100
   },
+  tabView: {
+    backgroundColor: '#f55'
+  },
+  indicator: {
+    backgroundColor: 'black',
+    width: 50,
+    height: 50
+  }
 });
